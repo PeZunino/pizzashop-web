646fb713ac2c6d758dfcb582124fdcf276292fb4
PeZunino <pedrozsantos@hotmail.com>
2025-01-07 14:33:54 -0300

    sign-in | sign-up

--- src/pages/auth/sign-up.tsx
+++ src/pages/auth/sign-up.tsx
@@ -1,6 +1,8 @@
+import { registerRestaurant } from "@/api/register-restaurant";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
+import { useMutation } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
@@ -16,6 +18,9 @@ const signUpForm = z.object({
 
 type SignUpForm = z.infer<typeof signUpForm>;
 
+const { mutateAsync: registerRestaurantFn } = useMutation({
+  mutationFn: registerRestaurant,
+});
 export function SignUp() {
   const navigate = useNavigate();
 
@@ -27,12 +32,17 @@ export function SignUp() {
 
   async function handleSignUp(data: SignUpForm) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await registerRestaurantFn({
+        restaurantName: data.restaurantName,
+        email: data.email,
+        managerName: data.managerName,
+        phone: data.phone,
+      });
 
       toast.success("Restaurante cadastrado com sucesso", {
         action: {
           label: "Login",
-          onClick: () => navigate("/sign-in"),
+          onClick: () => navigate(`/sign-in?email=${data.email}`),
         },
       });
     } catch (err) {
